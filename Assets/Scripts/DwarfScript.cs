c0bbf9fac786470770a84794419535bf699d1198
mortnord <mortnord@gmail.com>
2022-01-13 09:50:55 +0100

    La til collision detection og delte opp backgrounden i UI og active objekter.

--- Assets/Scripts/DwarfScript.cs
+++ Assets/Scripts/DwarfScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DwarfScript : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,10 +16,15 @@ public class DwarfScript : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+        /*
         Vector2 position = transform.position;
         position.x += 2 * horizontal * Time.deltaTime;
         position.y += 2 * vertical * Time.deltaTime;
-        transform.position = position;
+        transform.position = position;*/ 
+
+        
+        float moveByX = horizontal * 2;
+        float moveByY = vertical * 2;
+        rb.velocity = new Vector2(moveByX, moveByY);
     }
 }
