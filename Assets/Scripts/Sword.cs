ebc6055893a5de85eef68511ebad326014249f8a
mortnord <mortnord@gmail.com>
2022-01-16 20:49:05 +0100

    Copper og Mithril sprites lagt til.

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     public int value;
+    public Sprite[] spriteArray;
+    public SpriteRenderer spriteRenderer;    
     // Start is called before the first frame update
 
     public int quality; //0 = low, 1 = normal, 2 = high
     void Start()
     {
         value = (100 * quality) + 100;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); //Dette er spirit-renderen, vi kan bruke denne til å bytte sprites.
+        spriteRenderer.sprite = spriteArray[quality];
     }
 
     // Update is called once per frame
