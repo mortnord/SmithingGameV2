021e98e6138858713848b84c0db37d1bc0193976
mortnord <mortnord@gmail.com>
2022-01-18 22:58:48 +0100

    Preparation til Missions

---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    public int Time_remaining = 0;
+    public List<GameObject> Objects_to_complete_mission = new List<GameObject>();
+    // Start is called before the first frame update
+    
+    public Mission()
+    {
+
+    }
+    // Update is called once per frame
+    
+}
