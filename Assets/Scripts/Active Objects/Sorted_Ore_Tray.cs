73e0801b0b8b1a8bcdedea7f011507088db75ddf
mortnord <mortnord@gmail.com>
2022-01-30 17:46:57 +0100

    Bedre animasjon av Iron sorted stuff.

--- Assets/Scripts/Active Objects/Sorted_Ore_Tray.cs
+++ Assets/Scripts/Active Objects/Sorted_Ore_Tray.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Sorted_Ore_Tray : MonoBehaviour
 {
-    public int quality;
+    public int quality; 
     public List<GameObject> Ores_in_tray = new List<GameObject>();
 
-    public Sprite[] spriteArray;
+    public Sprite[] spriteArray_iron;
+    public Sprite[] spriteArray_copper;
+    public Sprite[] spriteArray_Mithril;
+
     public SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
@@ -19,15 +22,29 @@ public class Sorted_Ore_Tray : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //En mulighet å lage en array med plasser, der vi fyller en og en plass med en sprite når vi legger noe inni?
-        if (Ores_in_tray.Count > 0)//Sprite endring fra tomt til fult. 
+        if(quality == 1)
         {
-            quality = ((int)Ores_in_tray[0].GetComponent<Ore>().ore_quality) + 1;
-            spriteRenderer.sprite = spriteArray[quality];
-        }
-        if (Ores_in_tray.Count == 0) //Tilbake til tomt når det er tomt. 
-        {
-            spriteRenderer.sprite = spriteArray[0];
+            //En mulighet å lage en array med plasser, der vi fyller en og en plass med en sprite når vi legger noe inni?
+            if (Ores_in_tray.Count > 0 && Ores_in_tray.Count < 3)//Sprite endring fra tomt til fult. 
+            {
+                spriteRenderer.sprite = spriteArray_iron[1];
+            }
+            else if (Ores_in_tray.Count > 3 && Ores_in_tray.Count <= 6)
+            {
+                spriteRenderer.sprite = spriteArray_iron[2];
+            }
+            else if (Ores_in_tray.Count > 7 && Ores_in_tray.Count <= 9)
+            {
+                spriteRenderer.sprite = spriteArray_iron[3];
+            }
+            else if(Ores_in_tray.Count == 10)
+            {
+                spriteRenderer.sprite = spriteArray_iron[4];
+            }
+            else if (Ores_in_tray.Count == 0) //Tilbake til tomt når det er tomt. 
+            {
+                spriteRenderer.sprite = spriteArray_iron[0];
+            }
         }
     }
 }
